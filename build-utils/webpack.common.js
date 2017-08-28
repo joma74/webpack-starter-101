83d22c276965ea4defb1853cb56b371669cc7035
joma74 <dev-mails@gmx.net>
2017-08-28 15:19:35 +0200

    PICs are inlined by file size th

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -4,6 +4,19 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const config = {
     entry: "./src/main/js/",
+    module: {
+        rules: [
+            {
+                test: /\.(woff|png|jpg|gif)$/,
+                use: {
+                    loader: "url-loader",
+                    options: {
+                        limit: 10000
+                    }
+                }
+            }
+        ]
+    },
     output: {
         filename: "bundle.js",
         path: commonPaths.outputPath
--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,3 +1,5 @@
 /* eslint-env browser */
+import "../assets/SMALL.jpg";
+import "../assets/BIG.jpg";
 
 console.log("Hello webpack")
