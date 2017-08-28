716d7aef8c78086867c69f49f9898598cdbfa114
joma74 <dev-mails@gmx.net>
2017-08-28 14:42:21 +0200

    Added index.html generation

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -1,11 +1,17 @@
 const commonPaths = require("./common-paths");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const config = {
     entry: "./src/main/js/",
     output: {
         filename: "bundle.js",
         path: commonPaths.outputPath
-    }
+    },
+    plugins: [
+        new webpack.ProgressPlugin(),
+        new HtmlWebpackPlugin()
+    ]
 }
 
 module.exports = config;
