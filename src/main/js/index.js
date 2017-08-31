2bbfd425d072ba1de45b3720c65ff87c26eaae92
joma74 <dev-mails@gmx.net>
2017-09-01 01:01:54 +0200

    Wired up first Vue component

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -3,8 +3,18 @@
 import FOO_STRING from "./foo"
 import BAZ_STRING from "js@/components/baz"
 import "css@/index.css"
-import "css@/twitterlike.scss"
+
+import Vue from "vue";
+import TwitterLike from "js@/components/TwitterLike.vue"
 
 const bar = require("./bar")
 
 console.log(`Hello ${FOO_STRING} and hello ${bar.BAR_STRING} plus hello ${BAZ_STRING}`); // This is an ES6 feature
+
+new Vue({
+    el: "#twitterlike",
+    template: "<TwitterLike></TwitterLike>",
+    components: {
+        TwitterLike
+    }
+});
