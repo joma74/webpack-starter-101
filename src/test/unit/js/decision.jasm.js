a0d4587bd21d34e8915965ee2f0e9b802ec2c478
joma74 <dev-mails@gmx.net>
2017-10-20 01:49:08 +0200

    Initial !not compile clean

--- src/test/unit/js/decision.jasm.js
+++ src/test/unit/js/decision.jasm.js
@@ -1,47 +1,37 @@
-/*global fdescribe*/
-/*global fit*/
+/*global describe*/
 /*global it*/
 /*global expect*/
 
 "use strict";
 
-import { mergeValues, RPick } from "jsm@/utils/ManProps"
+import C from "jsm@/components/twitterlike/C"
+import BitSet from "jsm@/utils/BitSet"
+import {
+    apply as Rapply
+} from "ramda";
 
-fdescribe("decision", () => {
+describe("decision", () => {
 
-    it("can destructure selective into new object", () => {
-        const data = {
-            d_hasFocus: true,
-            d_hasHover: false,
-            d_dunno: null
-        }
-        /** https://stackoverflow.com/a/39333479 */
-        const facts_output = (({ d_hasFocus, d_hasHover }) => ({ d_hasFocus, d_hasHover }))(data); // eslint-disable-line no-undef
-        expect(facts_output["d_hasFocus"]).toBe(true);
-        expect(facts_output.d_hasHover).toBe(false);
-        expect(facts_output.d_dunno).toBeUndefined();
-    });
-
-    fit("can pick into new object", () => {
-        const data = {
-            d_hasFocus: true,
-            d_hasHover: false,
-            d_dunno: null
-        }
-        const facts_output = RPick(["d_hasFocus", "d_hasHover"], data);
-        expect(facts_output["d_hasFocus"]).toBe(true);
-        expect(facts_output.d_hasHover).toBe(false);
-        expect(facts_output.d_dunno).toBeUndefined();
-    });
-
-    fit("can destructure selective into existing object", () => {
-        const data = {
-            d_hasFocus: true,
-            d_hasHover: false
-        }
-        mergeValues({ d_hasFocus: false, d_notExistsInTo: true }, data); // eslint-disable-line no-undef
-        expect(data["d_hasFocus"]).toBe(false);
-        expect(data.d_hasHover).toBe(false);
-        expect(data.d_notExistsInTo).toBeUndefined();
+    it("can convert true-false array into a BitSet", () => {
+        /**
+         * @type {boolean[][]} 
+         */
+        let decisionTable = [
+            /* beautify preserve:start */
+            [          C.Y,            C.N,           C.N,            C.N ], //
+            [          C.N,            C.Y,           C.N,            C.N ], //
+            [          C.N,            C.N,           C.Y,            C.N ], //
+            [          C.N,            C.N,           C.N,            C.Y ] //
+            /* beautify preserve:end */
+        ];
+        const booleansToBinaryString = 
+        /**
+         * 
+         * @param {boolean[]} arrayOfBooleans 
+         * @returns {BitSet}
+         */
+        const transformToBitset = arrayOfBooleans => {arrayOfBooleans; return new BitSet("1000")};
+        const tBitSet = Rapply(transformToBitset, decisionTable[0]);
+        expect(tBitSet).toEqual(new BitSet("1000"));
     });
 });
